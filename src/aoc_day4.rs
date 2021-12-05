9418dbb83c8e24c0ba577944667c8042fa858191
visual studio <adityashiroor@gmail.com>
2021-12-05 12:07:32 +0530

    Completed day 5

--- src/aoc_day4.rs
+++ src/aoc_day4.rs
@@ -1,25 +1,4 @@
 use std::fs;
-const SAMPLE_INPUT: &str = r#"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26
-
-22 13 17 11  0
- 8  2 23  4 24
-21  9 14 16  7
- 6 10  3 18  5
- 1 12 20 15 19
-
- 3 15  0  2 22
- 9 18 13 17  5
-19  8  7 25 23
-20 11 10 24  4
-14 21 16 12  6
-
-14 21 17 24  4
-10 16 15  9 19
-18  8 23 26 20
-22 11 13  6  5
- 2  0 12  3  7"#;
-const DIAGONAL_LR: [usize; 5] = [0, 6, 12, 18, 24];
-const DIAGONAL_RL: [usize; 5] = [4, 8, 12, 16, 20];
 
 struct Board {
     data   : [[u32 ; 25]; 100],
@@ -27,6 +6,7 @@ struct Board {
     draws  : [u32; 100],
     won    : [bool; 100],
     done   : usize,
+    last   : usize,
 }
 
 impl Board {
@@ -37,6 +17,7 @@ impl Board {
             draws  : [0; 100],
             won    : [false; 100],
             done   : 0,
+            last   : 0,
         }
     }
 
@@ -50,7 +31,6 @@ impl Board {
         (self.data[board][y * 5 + x], self.marked[board][y * 5 + x])
     }
 
-    #[inline]
     fn check_win_array(&self, board: usize, array: [usize; 5]) -> bool {
         let mut win = false;
         let mut win_locations = [false; 5];
@@ -65,6 +45,7 @@ impl Board {
         win
     }
 
+    #[inline]
     fn check_win(&self, board: usize) -> bool {
         let mut win = false;
         for i in 0..2 {
@@ -118,11 +99,12 @@ impl Board {
         boards
     }
 
+    #[inline]
     fn next_win(&mut self) -> Option<(usize, u32)> {
         if self.done == 100 { return None; }
         let mut winning = None;
         let mut last_call = 0;
-        for i in 0..100 {
+        for i in self.last..100 {
             if winning.is_some() { break; }
             let drawn = &self.draws[..(i + 1)];
             for j in 0..100 {
@@ -140,6 +122,7 @@ impl Board {
                 if self.check_win(j) {
                     self.won[j] = true;
                     self.done += 1;
+                    self.last = i;
                     last_call = self.draws[i];
                     winning = Some((j, last_call));
                     break;
@@ -164,7 +147,7 @@ pub fn aoc_day4() {
     let string =
         fs::read_to_string("src/aoc-day4-input")
             .expect("Failed to read input file!");
-
+    let time = std::time::Instant::now();
     let mut boards = Board::generate_board(string);
     let (win, last_draw) = boards.next_win().unwrap();
     println!("Result of challenge 1: {}", boards.unmarked_value(win) * last_draw);
@@ -175,4 +158,5 @@ pub fn aoc_day4() {
         last_board_call = last_called;
     }
     println!("Result of challenge 2: {}", boards.unmarked_value(board) * last_board_call);
+    println!("Time: {:?}", time.elapsed());
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod aoc_day1;
 mod aoc_day2;
 mod aoc_day3;
 mod aoc_day4;
+mod aoc_day5;
 
 fn main() {
     // day 1
@@ -15,4 +16,7 @@ fn main() {
 
     // day 4
     aoc_day4::aoc_day4();
+
+    // day 5
+    aoc_day5::aoc_day5();
 }
