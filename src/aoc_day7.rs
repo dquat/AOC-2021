c529919b34c65dae01c12d53632434d8a89c9456
visual studio <adityashiroor@gmail.com>
2021-12-07 11:10:57 +0530

    Completed day 7.

---
+++ src/aoc_day7.rs
@@ -0,0 +1,40 @@
+use std::fs;
+use std::ops::Sub;
+
+const SAMPLE_INPUT: &str = "16,1,2,0,4,2,7,1,2,14";
+
+fn get_fuel(positions: &Vec<usize>, constant: bool) -> usize {
+    let &max = positions.iter().max().unwrap();
+    let &min = positions.iter().min().unwrap();
+    let mut min_fuel = usize::MAX;
+    for i in min..max {
+        let mut fuel = 0;
+        positions
+            .iter()
+            .for_each(|&p| {
+                if constant {
+                    fuel += isize::abs(i as isize - p as isize) as usize;
+                } else {
+                    fuel += (1..=isize::abs(i as isize - p as isize) as usize).sum::<usize>();
+                }
+            });
+        if fuel < min_fuel {
+            min_fuel = fuel;
+        }
+    }
+    min_fuel
+}
+
+pub fn aoc_day7() {
+    let string = //SAMPLE_INPUT.to_string();
+        std::fs::read_to_string("src/aoc-day7-input")
+            .expect("Failed to read input file!");
+    let positions =
+        string
+            .split(",")
+            .map(str::parse::<usize>)
+            .collect::<Result<Vec<_>, _>>()
+            .unwrap();
+    println!("Result for challenge 1: {}", get_fuel(&positions, true));
+    println!("Result for challenge 2: {}", get_fuel(&positions, false));
+}
--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,27 @@ mod aoc_day3;
 mod aoc_day4;
 mod aoc_day5;
 mod aoc_day6;
+mod aoc_day7;
 
 fn main() {
-    // day 1
-    aoc_day1::aoc_day1();
+    // // day 1
+    // aoc_day1::aoc_day1();
+    //
+    // // day 2
+    // aoc_day2::aoc_day2();
+    //
+    // // day 3
+    // aoc_day3::aoc_day3();
+    //
+    // // day 4
+    // aoc_day4::aoc_day4();
+    //
+    // // day 5
+    // aoc_day5::aoc_day5();
+    //
+    // // day 6
+    // aoc_day6::aoc_day6();
 
-    // day 2
-    aoc_day2::aoc_day2();
-
-    // day 3
-    aoc_day3::aoc_day3();
-
-    // day 4
-    aoc_day4::aoc_day4();
-
-    // day 5
-    aoc_day5::aoc_day5();
-
-    // day 6
-    aoc_day6::aoc_day6();
+    // day 7
+    aoc_day7::aoc_day7();
 }
