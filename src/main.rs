0a138fa44c120f478136a90eece614b1e68808d3
visual studio <adityashiroor@gmail.com>
2021-12-12 15:28:43 +0530

    Finished day 12.

--- src/main.rs
+++ src/main.rs
@@ -48,16 +48,16 @@ macro_rules! solve_days {
 fn main() {
     solve_days!(
         aoc_day12,
-        // aoc_day11,
-        // aoc_day10,
-         // aoc_day9,
-         // aoc_day8,
-         // aoc_day7,
-         // aoc_day6,
-         // aoc_day5,
-         // aoc_day4,
-         // aoc_day3,
-         // aoc_day2,
-         // aoc_day1,
+        aoc_day11,
+        aoc_day10,
+         aoc_day9,
+         aoc_day8,
+         aoc_day7,
+         aoc_day6,
+         aoc_day5,
+         aoc_day4,
+         aoc_day3,
+         aoc_day2,
+         aoc_day1,
     );
 }
